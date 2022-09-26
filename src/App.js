b6e0f51edbc24a0a8f05fff3a502c135fe1aea81
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-09-26 15:46:38 -0700

    refresh page to display book by id when clicked

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import BookList from './components/book/BookList';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Book from './components/book/Book.js';
+import Book from './components/book/Book';
+import BookDetail from './components/book/BookDetail';
 
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <Switch>
-        <Route exact path="/" component={BookList} />
-        <Route path="/books" component={Book} />
-      </Switch>
-    </main>
+    <Switch>
+      <Route path="/books/:id" component={BookDetail} />
+      <Route path="/books" component={Book} />
+      <Route path="/" component={BookList} />
+    </Switch>
   );
 }
 
